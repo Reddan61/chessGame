76f071e3bf5a09ef3c251010753d0e8029a6a9c2
Reddan61 <reddan61@yandex.ru>
2023-08-12 20:23:46 +0300

    show available by selected figure and move this figure, fix load figure images

--- src/utils/ChessBoard/Figures/Pawn.ts
+++ src/utils/ChessBoard/Figures/Pawn.ts
@@ -1,13 +1,19 @@
 import PawnSVG from "@svg/Pawn.svg";
 import { Figure } from "./Figure";
 
-type Payload = Omit<ConstructorParameters<typeof Figure>["0"], 'image'>;
-
+// type Payload = Omit<ConstructorParameters<typeof Figure>["0"], 'image'>;
 export class Pawn extends Figure {
-  constructor(payload: Payload) {
+  constructor() {
     super({
-        ...payload,
-        image: PawnSVG
+      image: PawnSVG,
     });
   }
+
+  public getDirections: Figure["getDirections"] = function () {
+    // TODO: сделать логику для первого хода
+    return [
+      [0, -1],
+      [0, -2],
+    ];
+  };
 }
